8cf559a6c79cbd6930887b8f9acc70792fd27671
Vasilchikov Aleksey <alekseyvi1303@gmail.com>
2018-07-20 10:20:43 +0300

    коммит для пула

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -140,30 +140,6 @@ $(document).ready(function () {
 		dots: true
 	});
 
-	// news carousel
-	$('.owl-news').owlCarousel({
-		margin: 20,
-		nav: true,
-		navText: ['', ''],
-		stagePadding: 10,
-		responsive: {
-			320: {
-				stagePadding: 20,
-				items: 1
-			},
-			768: {
-				stagePadding: 10,
-				items: 2
-			},
-			991: {
-				items: 3
-			},
-			1199: {
-				items: 4
-			}
-		}
-	});
-
 	// testimonials carousel
 	$('.carousel-testimonials').owlCarousel({
 		margin: 20,
